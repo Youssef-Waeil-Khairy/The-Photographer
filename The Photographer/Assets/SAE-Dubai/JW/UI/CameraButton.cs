aaad09a1b67c7174b471d76113c9d7e7d1a21740
JanWillem Goedvolk <caringsharing57@gmail.com>
2025-04-21 00:50:45 +0400

    Camera Shop Progress
    Camera Shop now has buttons to select cameras to look at and displays their info

---
+++ The Photographer/Assets/SAE-Dubai/JW/UI/CameraButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SAE_Dubai.Leonardo.CameraSys;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SAE_Dubai.JW.UI
+{
+    public class CameraButton : MonoBehaviour
+    {
+        [Header("Camera Settings")]
+        [SerializeField] CameraSettings cameraSettings;
+        
+        [Header("Item Shop")]
+        [SerializeField] private float price = 100f;
+        [SerializeField] Button _button;
+        
+        [Header("UI")]
+        [SerializeField] CameraInfoPanel _cameraInfoPanel;
+        [SerializeField] TMP_Text _name;
+        [SerializeField] TMP_Text _price;
+
+        private void Start()
+        {
+            _name.text = cameraSettings.modelName;
+            _price.text = $"Price: ${price}";
+        }
+
+        public void SelectCamera()
+        {
+            if (_cameraInfoPanel != null)
+            {
+                if (!_cameraInfoPanel.isActiveAndEnabled)
+                {
+                    _cameraInfoPanel.gameObject.SetActive(true);
+                }
+            }
+            _cameraInfoPanel.SetCameraInfo(cameraSettings);
+        }
+    }
+}
---
+++ The Photographer/Assets/SAE-Dubai/JW/UI/CameraInfoPanel.cs
@@ -0,0 +1,49 @@
+﻿using SAE_Dubai.Leonardo.CameraSys;
+using TMPro;
+using UnityEngine;
+
+namespace SAE_Dubai.JW.UI
+{
+    public class CameraInfoPanel : MonoBehaviour
+    {
+        [Header("Camera Item")]
+        public CameraSettings CamSettings;
+
+        [Header("Text Fields")] 
+        [SerializeField] private TMP_Text cameraName;
+        [SerializeField] private TMP_Text cameraManufacturer;
+        [SerializeField] private TMP_Text cameraAutoISO;
+        [SerializeField] private TMP_Text cameraISO;
+        [SerializeField] private TMP_Text cameraAutoWB;
+        [SerializeField] private TMP_Text cameraAutoFocus;
+        [SerializeField] private TMP_Text cameraAperture;
+        [SerializeField] private TMP_Text cameraAutoShutterSpeed;
+        [SerializeField] private TMP_Text cameraShutterSpeed;
+        [SerializeField] private TMP_Text cameraHasZoom;
+        [SerializeField] private TMP_Text cameraSensor;
+
+        public void SetCameraInfo(CameraSettings cameraSettings)
+        {
+            CamSettings = cameraSettings;
+            
+            ShowCameraInfo();
+        }
+
+        private void ShowCameraInfo()
+        {
+            cameraName.text = CamSettings.modelName;
+            cameraManufacturer.text = CamSettings.manufacturer;
+            
+            cameraAutoISO.text = CamSettings.hasAutoISO ? "Auto ISO" : "Manual ISO";
+            cameraISO.text = $"Base ISO: {CamSettings.baseISO} | Max ISO: {CamSettings.maxISO}";
+            
+            cameraHasZoom.text = CamSettings.hasZoom ? "Zoom Enabled" : "Zoom Disabled";
+            cameraAutoFocus.text = CamSettings.hasAutoFocus ? "Auto Focus" : "Manual Focus";
+            cameraAperture.text = $"Aperture: {CamSettings.minAperture} -> {CamSettings.maxAperture}";
+            
+            cameraAutoShutterSpeed.text = CamSettings.hasAutoShutterSpeed ? "Auto Shutter Speed" : "Manual Shutter Speed";
+            cameraShutterSpeed.text = $"Shutter Speed: {CamSettings.minShutterSpeed} -> {CamSettings.maxShutterSpeed}";
+            
+        }
+    }
+}
